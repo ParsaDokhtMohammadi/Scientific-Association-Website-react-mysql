8a226e61eb5cae23fb90c83d296ea2de99385322
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-06-02 15:03:30 +0330

    user and submission admin panel updated

--- client/src/pages/SubmissionAdmin.jsx
+++ client/src/pages/SubmissionAdmin.jsx
@@ -15,22 +15,22 @@ const SubmissionAdmin = () => {
   const { data: PendingSubmissions } = useGetPendingsubmissionsQuery();
   const [approveSubmission, { isSuccess, isError }] =
     useApproveSubmissionMutation();
-  const [rejectSubmission, {}] = useRejectSubmissionMutation();
+  const [rejectSubmission, {}] = useRejectSubmissionMutation(); 
   const [view, SetView] = useState("pending");
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex flex-col  w-full mx-auto  max-w-[80%] border  rounded ">
+      <div className="flex flex-col w-full mx-auto max-w-[80%] border  rounded bg-[#1A1A1A]">
         <div
           className={`flex flex-row items-center justify-around relative bg-[#1A1A1A] rounded-l rounded-r`}
         >
           <button
             className={`w-[50%] ${
               view === "pending"
-                ? "bg-[#1A1A1A] text-[#F5F5F5]  rounded-r-4xl relative left-7"
-                : "bg-[#A3A3A3] text-[#444] hover:bg-[#D4D4D4] hover:text-[#1A1A1A]"
+                ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-r-4xl relative left-7"
+                : "bg-[#A3A3A3] text-[#0D0D0D] hover:bg-[#F5F5F5] hover:text-[#0D0D0D]"
             }
-           py-4    cursor-pointer`}
+           py-4 cursor-pointer`}
             onClick={() => SetView("pending")}
           >
             pending submissions
@@ -38,64 +38,54 @@ const SubmissionAdmin = () => {
           <button
             className={`w-[50%] ${
               view === "history"
-                ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-l-4xl relative right-7 "
-                : "bg-[#A3A3A3] text-[#444] hover:bg-[#D4D4D4] hover:text-[#1A1A1A]"
+                ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-l-4xl relative right-7"
+                : "bg-[#A3A3A3] text-[#0D0D0D] hover:bg-[#F5F5F5] hover:text-[#0D0D0D]"
             }
-           py-4 rounded-rt   cursor-pointer`}
+           py-4 cursor-pointer`}
             onClick={() => SetView("history")}
           >
             submission history
           </button>
         </div>
         {view === "pending" && (
-          <table className="w-full border-collapse">
+          <table className="w-full border-collapse text-[#F5F5F5]">
             <thead>
               <tr>
-                <th className="border py-2 bg-sky-500">title</th>
-                <th className="border py-2 bg-sky-500 not-md:hidden">
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">title</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-md:hidden">
                   content
                 </th>
-                <th className="border py-2 bg-sky-500 not-md:hidden">
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-md:hidden">
                   user name
                 </th>
-                <th className="border py-2 bg-sky-500 not-lg:hidden">
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">
                   submitted at
                 </th>
-                <th className="border py-2 bg-sky-500">details</th>
-                <th className="border py-2 bg-sky-500 not-lg:hidden">
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">details</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">
                   approve
                 </th>
-                <th className="border py-2 bg-sky-500 not-lg:hidden">reject</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">reject</th>
               </tr>
             </thead>
             <tbody>
               {PendingSubmissions?.data.map((datum) => (
                 <tr key={datum.id}>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">
-                    {datum?.title}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-md:hidden">
-                    {datum?.content}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-md:hidden">
-                    {datum?.user_name}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">
-                    {(datum?.created_at).slice(0, 10)}
-                  </td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.title}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-md:hidden">{datum?.content}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-md:hidden">{datum?.user_name}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.created_at.slice(0, 10)}</td>
                   <td className="border py-2 px-2 max-w-[200px] truncate">
                     <button
-                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
-                      onClick={() =>
-                        navigate(`/SingleSubmissionPreview/${datum?.id}`)
-                      }
+                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
+                      onClick={() => navigate(`/SingleSubmissionPreview/${datum?.id}`)}
                     >
                       details
                     </button>
                   </td>
                   <td className="border py-2 px-2 max-w-[200px] truncate not-lg:hidden">
                     <button
-                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
+                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
                       onClick={() => approveSubmission(datum?.id)}
                     >
                       approve
@@ -103,7 +93,7 @@ const SubmissionAdmin = () => {
                   </td>
                   <td className="border py-2 px-2 max-w-[200px] truncate not-lg:hidden">
                     <button
-                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
+                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
                       onClick={() => rejectSubmission(datum?.id)}
                     >
                       reject
@@ -115,55 +105,38 @@ const SubmissionAdmin = () => {
           </table>
         )}
         {view === "history" && (
-          <table className="w-full border-collapse">
+          <table className="w-full border-collapse text-[#F5F5F5]">
             <thead>
               <tr>
-                <th className="border py-2 bg-sky-500">title</th>
-                <th className="border py-2 bg-sky-500 not-sm:hidden">
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D]">title</th>
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D] not-sm:hidden">
                   content
                 </th>
-                <th className="border py-2 bg-sky-500 not-lg:hidden">
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">
                   user name
                 </th>
-                <th className="border py-2 bg-sky-500 not-lg:hidden">
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">
                   submitted at
                 </th>
-                <th className="border py-2 bg-sky-500 not-md:hidden">status</th>
-                <th className="border py-2 bg-sky-500">details</th>
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D] not-md:hidden">status</th>
+                <th className="border border-[#F5F5F5] py-2 bg-[#06B6D4] text-[#0D0D0D]">details</th>
               </tr>
             </thead>
             <tbody>
               {AllSubmissions?.data.map((datum) => (
                 <tr key={datum.id}>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">
-                    {datum?.title}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-sm:hidden">
-                    {datum?.content}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">
-                    {datum?.user_name}
-                  </td>
-                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">
-                    {(datum?.created_at).slice(0, 10)}
-                  </td>
-                  <td
-                    className={`border py-2 px-2 text-center max-w-[200px] truncate border-[#F5F5F5] not-md:hidden ${
-                      datum?.status === "approved"
-                        ? "text-green-400"
-                        : datum?.status === "rejected"
-                        ? "text-red-500"
-                        : ""
-                    }`}
-                  >
-                    {datum?.status}
-                  </td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.title}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-sm:hidden">{datum?.content}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.user_name}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.created_at.slice(0, 10)}</td>
+                  <td className={`border border-[#F5F5F5] py-2 px-2 text-center max-w-[200px] truncate not-md:hidden ${
+                    datum?.status === "approved" ? "text-green-400" :
+                    datum?.status === "rejected" ? "text-red-500" : "text-[#A3A3A3]"
+                  }`}>{datum?.status}</td>
                   <td className="border py-2 px-2 max-w-[200px] truncate">
                     <button
-                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
-                      onClick={() =>
-                        navigate(`/SingleSubmissionPreview/${datum?.id}`)
-                      }
+                      className="lg:w-[86px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
+                      onClick={() => navigate(`/SingleSubmissionPreview/${datum?.id}`)}
                     >
                       details
                     </button>
--- client/src/pages/UsersAdmin.jsx
+++ client/src/pages/UsersAdmin.jsx
@@ -17,55 +17,66 @@ const UsersAdmin = () => {
 
   return (
     <>
-      <div className='flex flex-col w-full mx-auto max-w-[80%] border rounded '>
-        <div className={`flex flex-row items-center justify-around relative bg-[#1A1A1A] rounded-l rounded-r`}>
-          <button className={`w-[50%] ${view === "users" ? "bg-[#1A1A1A] text-[#F5F5F5]  rounded-r-lg relative left-7" : "bg-[#A3A3A3] text-[#444] hover:bg-[#D4D4D4] hover:text-[#1A1A1A]"} py-4 cursor-pointer`}
+      <div className="flex flex-col w-full mx-auto max-w-[80%] border rounded bg-[#1A1A1A]">
+        <div className="flex flex-row items-center justify-around relative bg-[#1A1A1A] rounded-l rounded-r">
+          <button
+            className={`w-[50%] ${view === "users"
+              ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-r-4xl relative left-7"
+              : "bg-[#A3A3A3] text-[#0D0D0D] hover:bg-[#F5F5F5] hover:text-[#0D0D0D]"} py-4 cursor-pointer`}
             onClick={() => SetView("users")}
           >
             All users
           </button>
-          <button className={`w-[50%] ${view === "members" ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-l-lg relative right-7" : "bg-[#A3A3A3] text-[#444] hover:bg-[#D4D4D4] hover:text-[#1A1A1A]"} py-4 cursor-pointer`}
+          <button
+            className={`w-[50%] ${view === "members"
+              ? "bg-[#1A1A1A] text-[#F5F5F5] rounded-l-4xl relative right-7"
+              : "bg-[#A3A3A3] text-[#0D0D0D] hover:bg-[#F5F5F5] hover:text-[#0D0D0D]"} py-4 cursor-pointer`}
             onClick={() => SetView("members")}
           >
-            admins and members
+            Admins and Members
           </button>
         </div>
 
         {view === "users" && (
-          <table className="w-full border-collapse">
+          <table className="w-full border-collapse text-[#F5F5F5]">
             <thead>
               <tr>
-                <th className='border py-2 bg-sky-500'>user name</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>email</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>password</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>joined at</th>
-                <th className='border py-2 bg-sky-500'>role</th>
-                <th className='border py-2 bg-sky-500'>make admin</th>
-                <th className='border py-2 bg-sky-500 '>member</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">user name</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">email</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">password</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">joined at</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">role</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">make admin</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">member</th>
               </tr>
             </thead>
             <tbody>
               {AllUsers?.data.map(datum => (
                 <tr key={datum.id}>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate'>{datum?.user_name}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{datum?.email}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{datum?.password}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{(datum?.created_at).slice(0, 10)}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate'>{datum?.role}</td>
-                  <td className='border py-2 px-2 max-w-[200px] truncate'>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.user_name}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.email}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.password}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{(datum?.created_at).slice(0, 10)}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.role}</td>
+                  <td className="border py-2 px-2 max-w-[200px] truncate">
                     <button
-                      className={`lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 cursor-pointer 
-                        ${datum?.role === "admin" ? "bg-gray-400 text-gray-700 cursor-not-allowed" : "bg-[#06B6D4] hover:bg-[#0891B2] text-white"}`}
+                      className={`lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 ${
+                        datum?.role === "admin"
+                          ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+                          : "bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D]"} cursor-pointer`}
                       onClick={() => PromoteToAdmin(datum?.id)}
                       disabled={datum?.role === "admin"}
                     >
                       make admin
                     </button>
                   </td>
-                  <td className='border py-2 px-2 max-w-[200px] truncate'>
-                    <button className="lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
+                  <td className="border py-2 px-2 max-w-[200px] truncate">
+                    <button
+                      className="lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
                       onClick={datum?.role !== "user" ? () => Demote(datum?.id) : () => Promote(datum?.id)}
-                    >{datum?.role !== "user" ? "demote" : "promote"}</button>
+                    >
+                      {datum?.role !== "user" ? "demote" : "promote"}
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -74,40 +85,45 @@ const UsersAdmin = () => {
         )}
 
         {view === "members" && (
-          <table className="w-full border-collapse">
+          <table className="w-full border-collapse text-[#F5F5F5]">
             <thead>
               <tr>
-                <th className='border py-2 bg-sky-500'>user name</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>email</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>password</th>
-                <th className='border py-2 bg-sky-500 not-lg:hidden'>joined at</th>
-                <th className='border py-2 bg-sky-500'>role</th>
-                <th className='border py-2 bg-sky-500'>make admin</th>
-                <th className='border py-2 bg-sky-500 '>member</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">user name</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">email</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">password</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D] not-lg:hidden">joined at</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">role</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">make admin</th>
+                <th className="border py-2 bg-[#06B6D4] text-[#0D0D0D]">member</th>
               </tr>
             </thead>
             <tbody>
               {AdminMembers?.data.map(datum => (
                 <tr key={datum.id}>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate'>{datum?.user_name}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{datum?.email}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{datum?.password}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden'>{(datum?.created_at).slice(0, 10)}</td>
-                  <td className='border py-2 px-2 text-center max-w-[200px] truncate'>{datum?.role}</td>
-                  <td className='border py-2 px-2 max-w-[200px] truncate'>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.user_name}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.email}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{datum?.password}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate not-lg:hidden">{(datum?.created_at).slice(0, 10)}</td>
+                  <td className="border py-2 px-2 text-center max-w-[200px] truncate">{datum?.role}</td>
+                  <td className="border py-2 px-2 max-w-[200px] truncate">
                     <button
-                      className={`lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 cursor-pointer 
-                        ${datum?.role === "admin" ? "bg-gray-400 text-gray-700 cursor-not-allowed" : "bg-[#06B6D4] hover:bg-[#0891B2] text-white"}`}
+                      className={`lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 ${
+                        datum?.role === "admin"
+                          ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+                          : "bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D]"} cursor-pointer`}
                       onClick={() => PromoteToAdmin(datum?.id)}
                       disabled={datum?.role === "admin"}
                     >
                       make admin
                     </button>
                   </td>
-                  <td className='border py-2 px-2 max-w-[200px] truncate'>
-                    <button className="lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#0891B2] cursor-pointer"
+                  <td className="border py-2 px-2 max-w-[200px] truncate">
+                    <button
+                      className="lg:w-[150px] rounded h-9 px-2 flex justify-center m-auto items-center duration-200 bg-[#06B6D4] hover:bg-[#A3A3A3] text-[#0D0D0D] cursor-pointer"
                       onClick={datum?.role !== "user" ? () => Demote(datum?.id) : () => Promote(datum?.id)}
-                    >{datum?.role !== "user" ? "demote" : "promote"}</button>
+                    >
+                      {datum?.role !== "user" ? "demote" : "promote"}
+                    </button>
                   </td>
                 </tr>
               ))}
